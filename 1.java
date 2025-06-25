ff557511ce6501e8c0d37735c36a4ed79b8ca0c0
sagar-46 <149888114+sagar-46@users.noreply.github.com>
2025-06-25 19:00:01 +0530

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,1 +1,13 @@
-
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the year: ");
+        int year = sc.nextInt();
+        if(year%400==0||(year%4==0&&year%100!=0)){
+            System.out.println("Entered year is Leap Year");
+        }else{
+            System.out.println("Not a Leap Year");
+        }
+    }
+}
