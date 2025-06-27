bf46e2ea5b6dd8180a4e60fb2faaeea48a7bc4ae
sagar-46 <149888114+sagar-46@users.noreply.github.com>
2025-06-27 21:13:26 +0530

    Update 3.java

--- 3.java
+++ 3.java
@@ -1,1 +1,14 @@
-
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter the number :");
+        int num = sc.nextInt();
+        int i = 1, fact = 1;
+        while (i<=num) {
+            fact *= i;
+            i++;
+        }
+        System.out.println(fact);
+    }
+}
