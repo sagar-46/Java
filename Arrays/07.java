3c4d10a3cb7d1a0d34f258ad8ab46eeaeee0aabf
Sagar D <sagardevaraj07@gmail.com>
2025-08-21 11:35:54 +0530

    Create 07.java

---
+++ Arrays/07.java
@@ -0,0 +1,78 @@
+import java.util.*;
+public class program {
+    public static int[] inputArray(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter the size of the Array :");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the Array Elements :");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+    public static void reverseArray(int [] arr, int i,int j){
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public static void rotate(int [] arr, int k){
+        int n = arr.length;
+        k = k%n;
+        reverseArray(arr, 0, n-k-1);
+        reverseArray(arr, n-k, n-1);
+        reverseArray(arr, 0, n-1);
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] arr = inputArray();
+        System.out.println("Enter the step to rotate :");
+        int k = sc.nextInt();
+        rotate(arr, k);
+        for (int i = 0; i < arr.lengthimport java.util.*;
+public class program {
+    public static int[] inputArray(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter the size of the Array :");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the Array Elements :");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+    public static int[] rotateArray(int[] arr, int k){
+        int n = arr.length;
+        int [] ans = new int[n];
+        int j = 0;
+        for (int i = n-k; i < ans.length; i++) {
+            ans[j++]=arr[i];
+        }
+        for (int i = 0; i < n-k; i++) {
+            ans[j++]=arr[i];
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = inputArray();
+        System.out.println("Enter the step to rotate :");
+        int k = sc.nextInt();
+        int [] ans = rotateArray(arr, k);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        sc.close();
+    }
+}; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
